60224838608e9125dbd553d1fc0f4ad3828f26de
Gecero <27288418+Gecero@users.noreply.github.com>
2019-02-24 20:24:03 +0100

    fixed upperCamelCase sorta typo

--- autowalk.cpp
+++ autowalk.cpp
@@ -23,8 +23,8 @@ const int findReportLength = 65;
 
 std::vector<find> finds;
 
-std::string read_file(const string& fname);
-void safe_file(std::string name, std::string contents, int offset, int size);
+std::string readFile(const string& fname);
+void safeFile(std::string name, std::string contents, int offset, int size);
 char getChar(const char * text, uint64_t index);
 void reportFind(find fnd);
 void debugPrint(bool print, std::string text);
@@ -34,7 +34,7 @@ bool isBase64(const char * text, size_t len);
 
 // thanks to @Jackojc#2309 for helping me out with file reading - btw: it's a modified version of his code
 // this function loads a whole file into a single string
-std::string read_file(const std::string& fname) {
+std::string readFile(const std::string& fname) {
     std::ifstream is(fname, std::ios::binary | std::ios::ate);
 
     // If file does not exist, return empty string and error.
@@ -56,7 +56,7 @@ std::string read_file(const std::string& fname) {
 }
 
 // save data from a specific offset to a file
-void safe_file(std::string name, std::string contents, int offset, int size) {
+void safeFile(std::string name, std::string contents, int offset, int size) {
 	std::string content = contents.substr(offset, size);
 	
 	std::ofstream ofs;
@@ -114,13 +114,13 @@ void printHelp() {
 // extract the data and then 
 void extractData(std::string & content) {
 	if(finds.size() == 1) {
-		safe_file("autowalk_found0" + finds[0].fileEnding, content, finds[0].offset, content.size());
-		safe_file("autowalk_found0.nfo", finds[0].type, 0, finds[0].type.length());
+		safeFile("autowalk_found0" + finds[0].fileEnding, content, finds[0].offset, content.size());
+		safeFile("autowalk_found0.nfo", finds[0].type, 0, finds[0].type.length());
 	} else if(finds.size() > 1) {
 		for(int i = 0; i < finds.size(); i++) {
 			int size = (i == finds.size() - 1) ? (content.length() - finds[i].offset) : finds[i+1].offset - finds[i].offset;
-			safe_file("autowalk_found" + std::to_string(i) + finds[i].fileEnding, content, finds[i].offset, size);
-			safe_file("autowalk_found" + std::to_string(i) + ".nfo", finds[i].type, 0, finds[i].type.length());		
+			safeFile("autowalk_found" + std::to_string(i) + finds[i].fileEnding, content, finds[i].offset, size);
+			safeFile("autowalk_found" + std::to_string(i) + ".nfo", finds[i].type, 0, finds[i].type.length());		
 		}
 	}
 }
@@ -182,7 +182,7 @@ int main(int argc, char * argv[]) {
 	}
 	
 	debugPrint(!quiet, "1/4    opening file..");
-	std::string content = read_file(file);
+	std::string content = readFile(file);
 	size_t length = content.size();
 	size_t progressInterval = pow(length, 0.75);
 	
